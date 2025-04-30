332935a7c88cf57655fa04d76051e47a79c30e74
ghjiukliop <152683933+ghjiukliop@users.noreply.github.com>
2025-04-30 21:02:31 +0700

    Update avangerx-test.lua

--- avangerx-test.lua
+++ avangerx-test.lua
@@ -623,14 +623,65 @@ local playerName = game:GetService("Players").LocalPlayer.Name
 
 -- Tạo Window
 local Window = Fluent:CreateWindow({
-    Title = "HT Hub | Anime Rangers X",
+    Title = "HT Hub | Arx Test",
     SubTitle = "",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = true,
-    Theme = ConfigSystem.CurrentConfig.UITheme or "Dark",
+    TabWidth = 140,
+    Size = UDim2.fromOffset(500, 400),
+    Acrylic = false,
+    Theme = "Amethyst",
     MinimizeKey = Enum.KeyCode.LeftControl
 })
+
+
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local CoreGui = game:GetService("CoreGui")
+local UserInputService = game:GetService("UserInputService")
+
+local MainUI = Fluent._screenGui
+local LogoUI = Instance.new("ScreenGui")
+LogoUI.Name = "HTHubLogo"
+LogoUI.Parent = CoreGui
+LogoUI.Enabled = false
+
+local LogoButton = Instance.new("ImageButton")
+LogoButton.Parent = LogoUI
+LogoButton.Size = UDim2.new(0, 50, 0, 50)
+LogoButton.Position = UDim2.new(0, 10, 0, 200)
+LogoButton.Image = "rbxassetid://7734056747"
+LogoButton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+LogoButton.BackgroundTransparency = 0
+LogoButton.Draggable = true
+LogoButton.Active = true
+
+local UICorner = Instance.new("UICorner", LogoButton)
+UICorner.CornerRadius = UDim.new(1, 0)
+
+local isMinimized = false
+
+Window.Minimize = function()
+    isMinimized = not isMinimized
+
+    if isMinimized then
+        MainUI.Enabled = false
+        LogoUI.Enabled = true
+    else
+        MainUI.Enabled = true
+        LogoUI.Enabled = false
+    end
+end
+
+LogoButton.MouseButton1Click:Connect(function()
+    isMinimized = false
+    MainUI.Enabled = true
+    LogoUI.Enabled = false
+end)
+
+UserInputService.InputBegan:Connect(function(input, gameProcessed)
+    if not gameProcessed and input.KeyCode == Enum.KeyCode.LeftControl then
+        Window.Minimize()
+    end
+end)
 
 -- Tạo tab Info
 local InfoTab = Window:AddTab({
